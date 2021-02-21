f1c3b099b07e1e93aadf0d3121a52eb9e125cd6f
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-02-21 18:29:11 +0100

    adds pages and links

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>Officia tempor pariatur non sit commodo culpa veniam. Amet minim irure nulla incididunt. Ea ullamco non enim adipisicing. Cupidatat occaecat laboris tempor velit irure dolor magna. Occaecat exercitation nostrud commodo pariatur nulla proident eu duis est minim labore amet sunt.</p>
+        <p>Amet ad aute nisi dolor minim. Esse laborum irure eu eiusmod tempor dolore. Ex quis aliqua deserunt ea nulla quis enim dolor ullamco consectetur. Tempor culpa ex quis excepteur consequat proident laboris sit laboris.</p>
+        <p>Qui tempor deserunt cillum amet cillum minim cupidatat anim quis aute dolore. Proident ex aliqua consectetur veniam reprehenderit et. Ipsum anim sunt dolore mollit. Aliqua eu consectetur eiusmod cillum ipsum consequat consectetur eiusmod minim excepteur proident in. Dolore cupidatat culpa quis qui consectetur enim esse sunt.</p>
+    </>
+);
+
+export default HomePage;
